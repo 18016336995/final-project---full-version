9e47fb163fbac361d87e768ca2387f54a2d148f0
P1uMa93 <104470956+P1uMa93@users.noreply.github.com>
2023-04-30 14:18:20 -0500

    Add files via upload

--- LED_drv.sv
+++ LED_drv.sv
@@ -6,29 +6,62 @@ module LED_drv(clk, rst_n, vld, lft_chnnl, rght_chnnl, LED);
     input signed [15:0] rght_chnnl;
     output logic [7:0] LED;
 
-    logic [15:0] inten; // music intensity, calculated by RMS value of left and right inputs
+    logic [15:0] inten, inten_temp; // music intensity, calculated by RMS value of left and right inputs
+    logic [31:0] sum, sum_lft, sum_lft_temp, sum_rght, sum_rght_temp;
+    logic done;
+
+    ///////////////////////////////////
+    // implement square root module //
+    /////////////////////////////////
+    sqrt iSQRT(.clk, .rst_n, .go(vld), .mag(sum), .sqrt(inten_temp), .done);
 
     //////////////////////////////////////
     // ff to calculate music intensity //
     ////////////////////////////////////
     always_ff @(posedge clk, negedge rst_n) begin
-        if(!rst_n) 
+        if(!rst_n) begin
+            sum <= 0;
             inten <= 0;
-        else 
-            if (vld) 
-                inten <= (lft_chnnl ^ 16'h8000) + (rght_chnnl ^ 16'h8000);
+        end
+        else begin 
+            sum <= sum_lft + sum_rght;
+            if (done)
+                inten <= inten_temp;
+        end
     end
+
+    always_ff @(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            sum_lft <= 0;
+            sum_lft_temp <= 0;
+        end
+        else if (vld) begin
+            sum_lft_temp <= lft_chnnl * lft_chnnl;
+            sum_lft <= sum_lft_temp;
+        end
+    end
+
+    always_ff @(posedge clk, negedge rst_n) begin
+        if(!rst_n) begin
+            sum_rght <= 0;
+            sum_rght_temp <= 0;
+        end
+        else if (vld) begin
+            sum_rght_temp <= lft_chnnl * lft_chnnl;
+            sum_rght <= sum_rght_temp;
+        end
+    end    
 
     //////////////////////////////////////////
     // LED effect based on music intensity //
     ////////////////////////////////////////
     assign LED[0] = (inten > 16'h0000) ? 1 : 0;
-    assign LED[1] = (inten > 16'h0100) ? 1 : 0;
-    assign LED[2] = (inten > 16'h0300) ? 1 : 0;
-    assign LED[3] = (inten > 16'h0400) ? 1 : 0;
-    assign LED[4] = (inten > 16'h0500) ? 1 : 0;
-    assign LED[5] = (inten > 16'h0600) ? 1 : 0;
-    assign LED[6] = (inten > 16'h0800) ? 1 : 0;
-    assign LED[7] = (inten > 16'h0900) ? 1 : 0;
+    assign LED[1] = (inten > 16'h0800) ? 1 : 0;
+    assign LED[2] = (inten > 16'h0b00) ? 1 : 0;
+    assign LED[3] = (inten > 16'h0e00) ? 1 : 0;
+    assign LED[4] = (inten > 16'h1100) ? 1 : 0;
+    assign LED[5] = (inten > 16'h1400) ? 1 : 0;
+    assign LED[6] = (inten > 16'h1700) ? 1 : 0;
+    assign LED[7] = (inten > 16'h2000) ? 1 : 0;
 
 endmodule
